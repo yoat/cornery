5a632dac38ed8db1d23952bc01273addc6f60025
M@ Senne <m@conjoinedcats.com>
2014-01-13 22:25:47 -0500

    First add.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cornery::Application.config.secret_key_base = '2b567a99fec8ca8d4720497cecb46b76dfd07b5e73f2d5bc05518bafa196cbb96a3aad531fa5bb458e34a8c4140fee6828d2bdd06f676a60e23d36c388a9c198'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cornery::Application.config.session_store :cookie_store, key: '_cornery_session'
